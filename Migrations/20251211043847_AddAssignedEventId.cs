90e9935154c424dd54da9607b784aee15110e2b7
m7mdtl3t <muhamedtalaatr@gmail.com>
2025-12-11 06:39:17 +0200

    "Add migration for AssignedEventId"

---
+++ Migrations/20251211043847_AddAssignedEventId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VivuqeQRSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssignedEventId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AssignedEventId",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AssignedEventId",
+                table: "Users",
+                column: "AssignedEventId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Events_AssignedEventId",
+                table: "Users",
+                column: "AssignedEventId",
+                principalTable: "Events",
+                principalColumn: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Events_AssignedEventId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_AssignedEventId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AssignedEventId",
+                table: "Users");
+        }
+    }
+}
